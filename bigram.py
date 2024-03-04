05757f775cc360cf8e4e9f088767e18bb6ba84d1
SIMPLY-CHAOTIC <jithinbaby5@gmail.com>
2024-03-04 21:39:59 +0530

    little more

--- bigram.py
+++ bigram.py
@@ -16,3 +16,19 @@ encode= lambda s:[string_to_int[c] for c in s]
 decode= lambda l:" ".join([int_to_string[i] for i in l])
 data=torch.tensor(encode(text),dtype=torch.long)
 print(data[:100])
+
+
+#train-test split
+n=int(0.8*len(data))
+train_data=data[:n]
+test_data=data[:n]
+
+#taking snippets of prediction and target
+block_size=8
+x=train_data[:block_size]
+y=train_data[1:block_size+1]
+
+for t in range(block_size):
+    context=x[:t+1]
+    target=y[t]
+    print("when input is",context,"target is",target)
